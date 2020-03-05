821f4ca54eecadd2333b4b5a8434f6ebc4997ed8
wustwxl <1175141062@qq.com>
2020-03-05 12:19:53 +0800

    zuul初次提交

---
+++ gateway-zuul/HELP.md
@@ -0,0 +1,19 @@
+#网关Zuul
+
+### Reference Documentation
+SpringBoot2.x搭建Zuul
+
+### Getting Started
+网关Zuul是Netflix出品的一个基于JVM路由和服务端的负载均衡器。
+Zuul功能：
+认证
+压力测试
+金丝雀测试
+动态路由
+负载削减
+安全
+静态响应处理
+主动/主动交换管理
+
+### Local Function
+暂无
---
+++ gateway-zuul/src/main/java/com/wust/gatewayzuul/GatewayZuulApplication.java
@@ -0,0 +1,17 @@
+package com.wust.gatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulApplication.class, args);
+    }
+
+}
