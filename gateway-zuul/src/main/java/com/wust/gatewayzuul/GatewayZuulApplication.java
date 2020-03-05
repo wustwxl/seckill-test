a443754f358eaf68ea79a7b76dfd3b5c2b026a13
wustwxl <1175141062@qq.com>
2020-03-05 12:25:16 +0800

    Zuul初次提交

--- gateway-zuul/src/main/java/com/wust/gatewayzuul/GatewayZuulApplication.java
+++ gateway-zuul/src/main/java/com/wust/gatewayzuul/GatewayZuulApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class GatewayZuulApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GatewayZuulApplication.class, args);
     }
 
