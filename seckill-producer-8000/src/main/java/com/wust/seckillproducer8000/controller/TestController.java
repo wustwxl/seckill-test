4433259b5a8434b4f4aed73d7fb1681ffc176cac
wustwxl <1175141062@qq.com>
2020-03-06 12:25:11 +0800

    测试web服务

--- seckill-producer-8000/src/main/java/com/wust/seckillproducer8000/controller/TestController.java
+++ seckill-producer-8000/src/main/java/com/wust/seckillproducer8000/controller/TestController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试web服务类
+ *
+ * @author wxl
+ * @date 2020.3.5
+ */
 @Slf4j
 @RestController
 @RequestMapping("/test")
